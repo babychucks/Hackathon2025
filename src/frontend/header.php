ecc053ea0f5c25c9b5d795c9bf95db0345dbfe78
Unathi <babychucks05@gmail.com>
2025-09-13 15:08:18 +0200

    added files and more docker

---
+++ src/frontend/header.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="assets/css/header.css">
+
+<header class="header">
+                <div class="header-container">
+                    <img src="img/logo.png" alt="Logo" class="pLogo">
+                    <nav class="navBar">
+                        <ul>
+                            <li id="signup-link"><a href="signup.php"><img src="img/SignUp.png" alt="Order"
+                                        class="icon"> <br> Sign Up</a></li>
+                            <li id="logout-link" style="display: none;"> <a href="products.php"><img src="img/Logout.png" alt="Login" class="icon"> <br> Logout</a></li>
+                            
+                            <li id="login-link"><a href="login.php"><img src="img/Login.png" alt="Login" class="icon">
+                                    <br> Login</a></li>
+                            <li id="products-link"><a href="products.php"><img src="img/produucts.png" alt="Order"
+                                        class="icon"> <br>Products</a></li>
+                            <li id="wishlist-link" style="display: none;"><a href="wishlist.php"><img src="img/wishlist.png" alt="Order"
+                                        class="icon"> <br> Wishlist</a></li>
+                            <li id="dashboard-link" style="display: none;"><a href="managerDashboard.php"><img src="img/dashboard.png" alt="Order"
+                                        class="icon"> <br>Dashboard</a></li>    
+                            <!-- Dashboard is a logged in feature? -->
+                        </ul>
+                    </nav>
+                </div>
+    </header>
